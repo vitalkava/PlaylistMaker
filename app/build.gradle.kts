5cd5359b3017d23c31ccd29590677873d81002ee
vitlkava <vitalikovalevich@gmail.com>
2025-04-05 22:32:27 +0300

    Homework 10 sprint (30.03.25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -45,4 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.glide)
+    kapt(libs.glide.compiler)
 }
--- app/src/main/java/com/example/playlistmaker/SearchAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/SearchAdapter.kt
@@ -1,28 +1,48 @@
 package com.example.playlistmaker
 
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.view.menu.MenuView.ItemView
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
-class SearchAdapter: RecyclerView.Adapter<SearchResultViewHolder> () {
+class SearchAdapter(
+    private val tracks: List<Track>
+): RecyclerView.Adapter<SearchResultViewHolder> () {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchResultViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.search_result_view)
-        return SearchResultViewHolder()
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.search_result_view, parent, false)
+        return SearchResultViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: SearchResultViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(tracks[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return tracks.size
     }
 }
 
 class SearchResultViewHolder(itemView: View): ViewHolder(itemView) {
 
+    private val albumImage: ImageView = itemView.findViewById(R.id.ivAlbumCard)
+    private val trackName: TextView = itemView.findViewById(R.id.tvSongName)
+    private val artistName: TextView = itemView.findViewById(R.id.tvBandName)
+    private val songDuration: TextView = itemView.findViewById(R.id.tvSongDuration)
+
+    fun bind(track: Track) {
+        trackName.text = track.trackName
+        artistName.text = track.artistName
+        songDuration.text = track.trackTime
+
+        val property = RequestOptions().transform(RoundedCorners(2))
+        Glide.with(itemView.context).load(track.artworkUrl100).placeholder(R.drawable.album_card_image).apply(property).into(albumImage)
+    }
 }
