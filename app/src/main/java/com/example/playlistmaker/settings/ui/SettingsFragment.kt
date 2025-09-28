b724f19e7fbdcc423e02aea770d45a6e99090063
vitalkava <vitalikovalevich@gmail.com>
2025-09-28 23:48:30 +0300

    19 sprint (SettingsActivity.kt -> SettingsFragment.kt)

--- app/src/main/java/com/example/playlistmaker/settings/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/settings/ui/SettingsFragment.kt
@@ -2,40 +2,41 @@ package com.example.playlistmaker.settings.ui
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.core.net.toUri
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.playlistmaker.R
 import com.example.playlistmaker.app.App
-import com.example.playlistmaker.databinding.ActivitySettingsBinding
+import com.example.playlistmaker.databinding.FragmentSettingsBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsFragment: Fragment() {
 
-    private lateinit var binding: ActivitySettingsBinding
+    private lateinit var binding: FragmentSettingsBinding
+
     private val viewModel: SettingsViewModel by viewModel()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivitySettingsBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
-        setContentView(binding.root)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        viewModel.isDarkTheme.observe(this) { isDark ->
+        viewModel.isDarkTheme.observe(viewLifecycleOwner) { isDark ->
             binding.themeSwitcher.isChecked = isDark
         }
 
         binding.themeSwitcher.setOnCheckedChangeListener { _, isChecked ->
             viewModel.toggleTheme(isChecked)
-            (applicationContext as App).switchTheme(isChecked)
+            (requireActivity().application as App).switchTheme(isChecked)
         }
 
         binding.buttonShareApp.setOnClickListener {
