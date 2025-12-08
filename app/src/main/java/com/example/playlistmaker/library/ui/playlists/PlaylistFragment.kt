e6a189d0f97c2688eccd74cd748b32dcae94d34d
vitalkava <vitalikovalevich@gmail.com>
2025-12-08 21:09:40 +0300

    23 sprint (second task (beginning))

---
+++ app/src/main/java/com/example/playlistmaker/library/ui/playlists/PlaylistFragment.kt
@@ -0,0 +1,36 @@
+package com.example.playlistmaker.library.ui.playlists
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.FragmentPlaylistBinding
+import java.util.zip.Inflater
+import java.util.zip.InflaterInputStream
+
+class PlaylistFragment : Fragment() {
+
+    private var _binding: FragmentPlaylistBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: PlaylistViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPlaylistBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonBack.setOnClickListener {
+            viewModel
+        }
+    }
+}
---
+++ app/src/main/java/com/example/playlistmaker/library/ui/playlists/PlaylistViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.playlistmaker.library.ui.playlists
+
+import androidx.lifecycle.ViewModel
+
+class PlaylistViewModel : ViewModel() {
+
+}
